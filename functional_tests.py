f9d3cc87066054fd8fa06ba0495e93ef48658138
Joshua Downes <j.adam.downes@gmail.com>
2017-01-01 04:13:08 -0500

    Use of python's unittest module for functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Get home page
+        self.browser.get('http://localhost:8000')
+
+        # Page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Enter to-do, check, enter, check
+
+        # Visit generated url
+
+if __name__ in '__main__':
+    unittest.main(warnings='ignore')
