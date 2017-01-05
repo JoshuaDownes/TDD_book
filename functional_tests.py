0636986b42063f1766000be6e2c81979fc7d35b3
Joshua Downes <j.adam.downes@gmail.com>
2017-01-04 23:20:50 -0500

    functional test additions, list stores previous input only

--- functional_tests.py
+++ functional_tests.py
@@ -9,7 +9,13 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        self.browser.quit()
+        #self.browser.quit()
+        pass
+
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Get home page
@@ -20,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # Enter to-do, check, enter, check
+        # Enter to-do, check
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -30,14 +36,17 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.check_for_row_in_table('1: Buy peacock feathers')
 
-        self.fail('Finish the test!')
+        # Enter a second to-do, check
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_table('1: Buy peacock feathers')
+        self.check_for_row_in_table('2: Use peacock feathers to make a fly')
+
+        self.fail('finish test!')
 
         # Visit generated url
 
