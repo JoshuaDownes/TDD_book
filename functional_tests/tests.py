8bb36cecd784dd3e43dcea01f20b60d0c839fe6e
Joshua Downes <j.adam.downes@gmail.com>
2017-01-06 13:50:59 -0500

    url refactor; self-contained urls in lists directory

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,8 +10,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        #self.browser.quit()
-        pass
+        self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
