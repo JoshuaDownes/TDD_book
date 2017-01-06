532b437e89765f3cd95eade8a9106cc8965d82c3
Joshua Downes <j.adam.downes@gmail.com>
2017-01-06 09:01:01 -0500

    integrate functional tests into django testing framework

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Get home page
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -49,6 +49,3 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('finish test!')
 
         # Visit generated url
-
-if __name__ in '__main__':
-    unittest.main(warnings='ignore')
