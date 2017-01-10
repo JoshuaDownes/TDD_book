5c31acf73441ad90c63df59c16d5c281baa86e27
Joshua Downes <j.adam.downes@gmail.com>
2017-01-09 19:42:14 -0500

    functional-test hacky workaround to test staging site; move database out of source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,19 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from lists.models import Item
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +29,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     
     def test_can_start_a_list_for_one_user(self):
         # Get home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # Page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -48,7 +58,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Start new list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
@@ -61,7 +71,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # New user; make sure no sign of old user
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -84,7 +94,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # User visits home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         # Make sure input box is approx centered
