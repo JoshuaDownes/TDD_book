1dbb774f35288f90da9b495323c9d8c9f746b525
Joshua Downes <j.adam.downes@gmail.com>
2017-01-11 02:07:15 -0500

    update allowed hosts

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(e)jhz@!=)rh52v3r@n*wb=847)n9#(3)(twoav+y!=ifjpx3o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lists-staging.terklerk.club']
 
 
 # Application definition
